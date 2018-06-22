5e2022a3978dd33dc116767cf258fd977c8601b5
Michael Adam <madamt0001@gmail.com>
2018-06-22 14:06:29 +0800

    Added update

--- database/migrations/2018_06_22_055613_create_subjects_table.php
+++ database/migrations/2018_06_22_055613_create_subjects_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProgramsTable extends Migration
+class CreateSubjectsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateProgramsTable extends Migration
      */
     public function up()
     {
-        Schema::create('programs', function (Blueprint $table) {
+        Schema::create('subjects', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->nullable();
-            $table->string('code')->nullable();
-            $table->string('description')->nullable();
-            $table->float('tuition_fee', 8, 2);
             $table->timestamps();
         });
     }
@@ -30,6 +26,6 @@ class CreateProgramsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('programs');
+        Schema::dropIfExists('subjects');
     }
 }
