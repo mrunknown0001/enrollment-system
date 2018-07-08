1e8d69d60f15f1837eca3c6b3cd2eb37c13bbe48
Michael Adam <madamt0001@gmail.com>
2018-07-09 01:07:03 +0800

    UPdate

--- resources/views/student/includes/assessment-payment.blade.php
+++ resources/views/student/includes/assessment-payment.blade.php
@@ -2,5 +2,5 @@
 	{{ csrf_field() }}
 	<input type="hidden" name="amount" value="{{ $assessment->total }}">
 	<input type="hidden" name="code" value="{{ $assessment->assessment_number }}">
-	<button class="btn" style="background-color:transparent" type="submit"><img src="{{ asset('uploads/imgs/paypal_pay_now.png') }}" height="35px" width="150px"></button>
+	<button class="btn" style="background-color:transparent" type="submit"><img src="{{ asset('uploads/imgs/paypal_pay_now.png') }}" height="35px" width="175px"></button>
 </form>
