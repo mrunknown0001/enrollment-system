0655e26888eeaaa676276aa23f30f55b39f6f0cb
Michael Adam <madamt0001@gmail.com>
2018-07-18 21:06:15 +0800

    update

--- resources/views/admin/rates-fees.blade.php
+++ resources/views/admin/rates-fees.blade.php
@@ -13,8 +13,9 @@
 @include('includes.all')
 
 <div class="row">
-	<div class="col-md-8">
+	<div class="col-md-12">
 		<p>Current Unit Price: <strong>&#8369; {{ $unit_price->price }}</strong> <a href="{{ route('admin.price.per.unit.update') }}" class="btn btn-primary"><i class="fa fa-pencil"></i> Update Price Per Unit</a></p>
+		<hr>
 		<p><a href="{{ route('admin.add.misc.fee') }}" class="btn btn-primary"><i class="fa fa-plus"></i> Add Miscellaneous Fee</a></p>
 		
 		@if(count($misc) > 0)
