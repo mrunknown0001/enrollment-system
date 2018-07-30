83dc085cf6f60b68f4dc2f3c7b24d16b031c18c2
Michael Adam <madamt0001@gmail.com>
2018-07-30 20:07:52 +0800

    fixing seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -101,8 +101,71 @@ class DatabaseSeeder extends Seeder
             'limit' => 40
         ]);
 
+        DB::table('grade_equivalents')->insert([
+            [
+                'equivalent' => 1.0,
+                'from' => 99,
+                'to' => 100,
+                'description' => 'Excellent'
+            ],
+            [
+                'equivalent' => 1.25,
+                'from' => 95,
+                'to' => 98,
+                'description' => 'Very Good'
+            ],
+            [
+                'equivalent' => 1.5,
+                'from' => 92,
+                'to' => 94,
+                'description' => 'Very Good'
+            ],
+            [
+                'equivalent' => 1.75,
+                'from' => 89,
+                'to' => 91,
+                'description' => 'Very Good'
+            ],
+            [
+                'equivalent' => 2.0,
+                'from' => 86,
+                'to' => 88,
+                'description' => 'Satisfactory'
+            ],
+            [
+                'equivalent' => 2.25,
+                'from' => 83,
+                'to' => 85,
+                'description' => 'Satisfactory'
+            ],
+            [
+                'equivalent' => 2.5,
+                'from' => 80,
+                'to' => 82,
+                'description' => 'Satisfactory'
+            ],
+            [
+                'equivalent' => 2.75,
+                'from' => 77,
+                'to' => 79,
+                'description' => 'Fair'
+            ],
+            [
+                'equivalent' => 3.0,
+                'from' => 75,
+                'to' => 76,
+                'description' => 'Fair'
+            ],
+            [
+                'equivalent' => 5.0,
+                'from' => 74,
+                'to' => 0,
+                'description' => 'Failed'
+            ]
+        ]);
+
         $this->call([
-            GradeEquivalentTableSeeder::class,
+            // EquivalentTableSeeder::class,
             SubjectTableSeeder::class,
             CourseTableSeeder::class,
             ProgramTableSeeder::class,
