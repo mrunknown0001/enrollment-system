646ebf2605b863ac3333d0928bd728796a76b89e
Michael Adam <madamt0001@gmail.com>
2018-10-05 02:08:59 +0800

    Update

--- resources/views/cashier/assessment-details.blade.php
+++ resources/views/cashier/assessment-details.blade.php
@@ -24,7 +24,7 @@
                     @include('includes.all')
                     <button id="hideOnPrint" type="button" onclick="window.print()" class="btn btn-primary"><i class="fa fa-print"></i> Print</button>
                     <div id="printableArea">
-                        <h3 class="text-center showOnPrint">Internation Computer Technology Colleges</h3>
+                        <h3 class="text-center showOnPrint">International Computer Technology Colleges</h3>
                         <p class="text-center showOnPrint">Your Gateway to a Global Job Opportunity</p>
                         <p class="text-center showOnPrint">2<sup>nd</sup> of AA building, Barangay Sto Cristo, Tarlac City</p>
                         
