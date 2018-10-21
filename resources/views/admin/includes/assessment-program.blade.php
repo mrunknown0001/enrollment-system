e19f2b9a275d3b73960b230a123b46ebb2183dd5
Adam <webdevsolutions.ph@gmail.com>
2018-10-21 23:15:20 +0800

    Update on print

--- resources/views/admin/includes/assessment-program.blade.php
+++ resources/views/admin/includes/assessment-program.blade.php
@@ -1,3 +1,7 @@
+<h3 class="text-center showOnPrint">International Computer Technology Colleges</h3>
+<p class="text-center showOnPrint">Your Gateway to a Global Job Opportunity</p>
+<p class="text-center showOnPrint">2<sup>nd</sup> of AA building, Barangay Sto Cristo, Tarlac City</p>
+
 <p><strong>{{ ucwords($assessment->student->firstname . ' ' . $assessment->student->lastname) . ' - ' . $assessment->student->student_number }}</strong></p>
 <p><strong>{{ $assessment->program->title }}</strong></p>
 <p>Tuition Fee: <strong>&#8369; {{ $assessment->tuition_fee }}</strong></p>
